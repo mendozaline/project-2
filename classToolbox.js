a3091685e220e17219abb7e5463856eedf2872f8
David <d_men2a@yahoo.com>
2016-11-14 19:38:56 -0800

    fix submodule

--- classToolbox.js
+++ classToolbox.js
@@ -109,9 +109,9 @@
         return new cToolbox.Vector(x, y);
     };
     
-    exports.weekDay = (function(exports) {
-        var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+    cToolbox.weekDay = (function(exports) {
+        var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",                "Friday", "Saturday"];
+        
         exports.name = function(number) {
             return names[number];
         };
@@ -119,11 +119,15 @@
         exports.number = function(name) {
             return names.indexOf(name);
         };
-    })(this.weekDay = {});
+        
+    })(typeof window === "undefined" || window === null ? global.weekDay = {} : window.weekDay = {} );
     
 })(typeof window === "undefined" || window === null ? global.cToolbox = {} : window.cToolbox = {} );
 
-//console.log(cToolbox.isPrime(11)); //true
-//console.log(cToolbox.buildRange(1, 5)); //[1, 2, 3, 4, 5]
-//console.log(cToolbox.calcSum(cToolbox.buildRange(1, 4))); //10
-//console.log(new cToolbox.Vector(1, 2).plus(new cToolbox.Vector(2, 3)));
+console.log(cToolbox.isPrime(11)); //true
+console.log(cToolbox.buildRange(1, 5)); //[1, 2, 3, 4, 5]
+console.log(cToolbox.calcSum(cToolbox.buildRange(1, 4))); //10
+console.log(new cToolbox.Vector(1, 2).plus(new cToolbox.Vector(2, 3)));
+console.log(weekDay.name(0)); //Sunday
+console.log(weekDay.number("Monday")); //1
+
